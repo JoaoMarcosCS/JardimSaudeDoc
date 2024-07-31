0be1a38dd247fc8413b9fee41d47af84445a9627
João Marcos <jmcsjoaomarcos@gmail.com>
2024-07-31 15:25:57 -0300

    Atualizar o style.ts

--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -6,7 +6,7 @@ export const Section = styled.section`
     background-color: var(--primary-white);
     flex-wrap: wrap;
     @media (max-width: 1100px){
-        padding: 0px 1rem;
+        padding: 0px 0.5rem;
         padding-top:30vh;
     }
 `
